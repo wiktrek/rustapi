35ad6813b5b9de9461e60f16e1207f24fd76a555
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-06 21:44:20 +0100

    .

--- src/routes/db.rs
+++ src/routes/db.rs
@@ -1,12 +1,20 @@
+
 use rocket::*;
 use dotenv;
+use ::serde::{Deserialize, Serialize};
 use mongodb::{Client, options::ClientOptions};
-#[get("/db/<option>/<data>")]
-pub fn db<'a>(option: &str, data: &str) -> &'a str {
+use exitfailure::ExitFailure;
+#[derive(Debug, Serialize, Deserialize)]
+struct File {
+    title: String,
+    data: String,
+}
+#[get("/db/<option>/<title>/<data>")]
+pub fn db<'a>(option: &str, title: &str, data: &str) -> &'a str {
 match option {
     "save"=> {
     let rt = tokio::runtime::Runtime::new().unwrap();
-    let future = save(data);
+    let future = save(data, title);
     rt.block_on(future);
         return "save"
     },
@@ -17,12 +25,16 @@ match option {
 _ => return "choose one option save or delete"
 }
 }
-async fn save(data: &str) {
+async fn save(data: &str, title: &str) -> Result<(),  ExitFailure> {
 let mut client_options = ClientOptions::parse(dotenv::var("DB").unwrap()).await?;
+let client = Client::with_options(client_options)?;
+let database = client.database("rust");
+
+
 println!("save");
 println!("{}", dotenv::var("DB").unwrap());
 
-
+Ok(())
 }
 fn delete(data: &str) {
 
