c9a42484853335c9cb6dc008b6df4726c421a967
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-07 22:40:31 +0100

    .

--- src/routes/db.rs
+++ src/routes/db.rs
@@ -18,8 +18,9 @@ match option {
         return "save"
     },
     "delete"=> {
-        delete(data);
-   return "delete"
+     let handle = tokio::runtime::Handle::try_current().unwrap();
+    handle.spawn(delete(data.to_string(), title.to_string()));
+        return "delete"
     },
 _ => return "choose one option save or delete"
 }
@@ -46,6 +47,24 @@ println!("{}", dotenv::var("DB").unwrap());
 
 Ok(())
 }
-fn delete(data: &str) {
-println!("{}",data)
+async fn delete(data: String, title: String) -> Result<(),  ExitFailure>{
+let client = Client::with_uri_str(dotenv::var("DB").unwrap()).await?;
+let database = client.database("rust");
+let collection = database.collection::<File>("files");
+println!("{:?}", collection);
+let docs = vec![
+    File{
+        title: title.to_string(),
+        data: data.to_string(),
+    }
+];
+collection.insert_many(docs, None).await?;
+// let cursor = collection.find(doc! { "title": title.to_string()}, None).await?;
+// for result in cursor {
+//     println!("title: {}", result?.title);
+// }
+println!("zapisano??");
+println!("{}", dotenv::var("DB").unwrap());
+
+Ok(())
 }
