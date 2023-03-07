ac048b074d848e26afcd694a48b5b127283922c0
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-07 22:38:39 +0100

    You can now save data

--- src/routes/db.rs
+++ src/routes/db.rs
@@ -13,8 +13,8 @@ pub fn db<'a>(option: &str, title: &str, data: &str) -> &'a str {
     println!("{}, {}, {}", option, title, data);
 match option {
     "save"=> {
-    let rt = tokio::runtime::Runtime::new().unwrap();
-    rt.spawn(save(data.to_string(), title.to_string()));
+    let handle = tokio::runtime::Handle::try_current().unwrap();
+    handle.spawn(save(data.to_string(), title.to_string()));
         return "save"
     },
     "delete"=> {
@@ -24,10 +24,12 @@ match option {
 _ => return "choose one option save or delete"
 }
 }
+
 async fn save(data: String, title: String) -> Result<(),  ExitFailure> {
 let client = Client::with_uri_str(dotenv::var("DB").unwrap()).await?;
 let database = client.database("rust");
 let collection = database.collection::<File>("files");
+println!("{:?}", collection);
 let docs = vec![
     File{
         title: title.to_string(),
@@ -39,7 +41,7 @@ collection.insert_many(docs, None).await?;
 // for result in cursor {
 //     println!("title: {}", result?.title);
 // }
-println!("save");
+println!("zapisano??");
 println!("{}", dotenv::var("DB").unwrap());
 
 Ok(())
