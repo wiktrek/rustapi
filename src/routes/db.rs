c44514782fe330ab6d06a3e554bc3b2785c9b3d9
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-08 18:49:32 +0100

    errors

--- src/routes/db.rs
+++ src/routes/db.rs
@@ -4,40 +4,54 @@ use ::serde::{Deserialize, Serialize};
 use mongodb::{ Client,bson::doc};
 use exitfailure::ExitFailure;
 #[derive(Debug, Serialize, Deserialize)]
-struct File {
+struct Files {
     title: String,
     data: String,
-}
-#[get("/db/<option>/<title>/<data>")]
-pub fn db<'a>(option: &str, title: &str, data: &str) -> &'a str {
-    println!("{}, {}, {}", option, title, data);
-match option {
-    "save"=> {
-    let handle = tokio::runtime::Handle::try_current().unwrap();
-    handle.spawn(save(data.to_string(), title.to_string()));
-        return "save"
-    },
-    "delete"=> {
-     let handle = tokio::runtime::Handle::try_current().unwrap();
-    handle.spawn(delete(data.to_string(), title.to_string()));
-        return "delete"
-    },
-_ => return "choose one option save or delete"
-}
+    id: String,
 }
 
-async fn save(data: String, title: String) -> Result<(),  ExitFailure> {
+#[derive(Debug, Serialize, Deserialize)]
+struct User {
+    name: String,
+    password: String,
+    id: String,
+    files: Vec<Files>
+}
+
+// pub fn db<'a>(option: &str, title: &str, data: &str) -> &'a str {
+//     println!("{}, {}, {}", option, title, data);
+// match option {
+//     "save"=> {
+//     let handle = tokio::runtime::Handle::try_current().unwrap();
+//     handle.spawn(save(data.to_string(), title.to_string()));
+//         return "save"
+//     },
+//     "delete"=> {
+//      let handle = tokio::runtime::Handle::try_current().unwrap();
+//     handle.spawn(delete(data.to_string(), title.to_string()));
+//         return "delete"
+//     },
+// _ => return "choose one option save or delete"
+// }
+// }
+#[get("/db/save/<username>/<password>/<id>/<title>/<data>")]
+pub async fn save<'r,'a>(username: &str, password: &str, id: &str, title: &str, data: &str) -> Result<(),  ExitFailure> {
 let client = Client::with_uri_str(dotenv::var("DB").unwrap()).await?;
 let database = client.database("rust");
-let collection = database.collection::<File>("files");
+let collection = database.collection::<User>("files");
 println!("{:?}", collection);
-let docs = vec![
-    File{
-        title: title.to_string(),
-        data: data.to_string(),
-    }
-];
-collection.insert_many(docs, None).await?;
+let user = User {
+    name: username.to_string(),
+    password: password.to_string(),
+    id: id.to_string(),
+    files: vec![Files {
+    title: title.to_string(),
+    data: data.to_string(),
+    id: id.to_string()
+
+    }]
+};
+collection.insert_one(user, None);
 // let cursor = collection.find(doc! { "title": title.to_string()}, None).await?;
 // for result in cursor {
 //     println!("title: {}", result?.title);
@@ -47,18 +61,12 @@ println!("{}", dotenv::var("DB").unwrap());
 
 Ok(())
 }
-async fn delete(data: String, title: String) -> Result<(),  ExitFailure>{
+#[get("/db/delete/<id>/<fileid>")]
+pub async fn delete(id: &str, fileid: &str) -> Result<(),  ExitFailure>{
 let client = Client::with_uri_str(dotenv::var("DB").unwrap()).await?;
 let database = client.database("rust");
-let collection = database.collection::<File>("files");
-println!("{:?}", collection);
-let docs = vec![
-    File{
-        title: title.to_string(),
-        data: data.to_string(),
-    }
-];
-collection.delete_many(docs, ).await?;
+let collection = database.collection::<User>("files");
+collection.delete_one().await?;
 // let cursor = collection.find(doc! { "title": title.to_string()}, None).await?;
 // for result in cursor {
 //     println!("title: {}", result?.title);
--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -1,4 +1,5 @@
 mod index;
 pub mod db;
 pub use index::index as index_rs;
-pub use db::db as db_rs;
+pub use db::save;
+pub use db::delete;
