cc9c8d4844a30f8468f54b603487ea9ea403a136
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-13 23:05:28 +0100

    .

--- src/routes/redirect.rs
+++ src/routes/redirect.rs
@@ -4,18 +4,16 @@ use rocket::serde::{json::Json, Deserialize, Serialize};
 use reqwest::{self, get};
 use serde_json::Value;
 use std::fs;
-#[derive(Deserialize, Serialize)]
-pub struct RedirectData {
+#[derive(Deserialize, Serialize, Debug)]
+struct Links {
     name: String,
-    redirect_url: String,
-    status: String,
+    redirects: String,
 }
-
 #[get("/redirect/<link>")]
 pub async fn redirect(link: String) -> String{
     let redirect_uri= uri!("https://wiktrek.xyz");
     let result = fs::read_to_string("src/data/redirect.json").unwrap();
 println!("{:?}", result);
 // Redirect::to(redirect_uri)
-format!("{:?}", serde_json::from_str(result.as_str()))
+format!("{:?}", serde_json::from_str::<Links>(result.as_str()).unwrap())
 }
