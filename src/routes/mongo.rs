61ded18ec19e12e0c30cf8ea6666bac0f9dbc57b
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-03-19 22:56:18 +0100

    idk

---
+++ src/routes/mongo.rs
@@ -0,0 +1,45 @@
+#![allow(unused)]
+use std::env;
+use bson::doc;
+use dotenv::dotenv;
+use rocket::*;
+use rocket::serde::{json::Json, Deserialize, Serialize};
+use rocket::{ http::Status};
+use mongodb::{ Client};
+
+#[derive(Deserialize, Serialize)]
+pub struct User{
+    username: String,
+    password: String,
+    id: String,
+}
+#[derive(Deserialize, Serialize)]
+pub struct Response{
+    response: String,
+    status: String,
+}
+#[get("/user/create/<username>/<password>")]
+pub async fn user_create(username: String, password: String) -> Result<Json<Response>, Status>{
+     dotenv().ok();
+                 let uri = match env::var("DB") {
+                Ok(v) => v.to_string(),
+                Err(_) => format!("Error loading env variable"),
+            };
+            let client = Client::with_uri_str(uri).await.unwrap();
+            println!("client: {:?}", client);
+            let database = client.database("rust");
+            let collection = database.collection::<User>("users");
+    let user = User {
+        username: username,
+        password: password,
+        id: "wiktrek".to_string(),
+};
+    collection.insert_one(user, None);
+    let response = Response {
+    response: "DATA WRITTEN to".to_string(),
+    status: "200".to_string(),
+};
+
+
+Ok(Json(response))
+}
