984815d2165250bc1310f46d563e7c1096332771
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-20 13:47:34 +0200

    selecction sort

--- src/routes/sort.rs
+++ src/routes/sort.rs
@@ -19,10 +19,41 @@ pub fn sort(alg: String, data: String) -> Json<Ping>{
 match algstr {
   "rust" => rust_sort(vec_i32),
   "simple" => simple_sort(vec_i32),
+  "selection" => selection_sort(vec_i32),
   _ => return Json(Ping { 
     response: format!("Error: couldn't find sorting algorithm {}", algstr),
     status: "400".to_string()})
 }
+}
+fn loc_of_smallest(v: Vec<i32>, mut i: usize, n: usize) -> usize{
+let mut j = i;
+while i<=n {
+  if v[i] < v[j] {
+    j = i;
+  }
+  i += 1;
+}
+return j
+}
+fn swap(mut v: Vec<i32>, x: usize, y: usize) -> Vec<i32>{
+  let temp = v[x];
+  v[x] = v[y];
+  v[y] = temp;
+return v;
+}
+fn selection_sort(mut v: Vec<i32>) -> Json<Ping> {
+    let mut i = 0;
+    let n: usize = v.len();
+    while i<n-1 {
+      let j = loc_of_smallest(v.clone(), i, n-1);
+      v = swap(v.clone(),i,j);
+      i +=1
+    }
+
+    Json(Ping {
+    response: format!("sorted: {:?}", v),
+    status: "200".to_string()
+  })
 }
 fn simple_sort(vec: Vec<i32>) -> Json<Ping> {
   let mut sorted = vec;
