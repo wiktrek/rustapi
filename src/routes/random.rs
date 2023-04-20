72a42804fe7d361e04cf0df6a7a9442f8a017289
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-20 14:47:51 +0200

    sort.rs + random.rs

--- src/routes/random.rs
+++ src/routes/random.rs
@@ -1,5 +1,13 @@
-use rand::prelude::*;
-#[get("/random/<amount>")]
-pub fn random(amount: i32) -> String{ 
-
+use rand::Rng;
+#[get("/random/<amount>/<min>/<max>")]
+pub fn random(amount: i32, min: i32, max: i32) -> String{
+    let mut nums: Vec<i32> = vec![];
+    let mut i = 0;
+    while i < amount {
+    let mut rng = rand::thread_rng();
+    let num: i32 = rng.gen_range(min..max);
+    nums.push(num);
+    i += 1;
+    }
+    format!("{:?}", nums).replace(" ", "").replace("[", "").replace("]", "")
 }
