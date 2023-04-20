e3f252d969a94ea8ed167ef230e04ad0a628e126
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-20 13:09:44 +0200

    .

--- src/routes/sort.rs
+++ src/routes/sort.rs
@@ -5,19 +5,31 @@ pub struct Ping {
     response: String,
     status: String,
 }
-#[get("/sort/<data>")]
-pub fn sort(data: String) -> Json<Ping>{
+#[get("/sort/<alg>/<data>")]
+pub fn sort(alg: String, data: String) -> Json<Ping>{
   let vec: Vec<&str> = data.split(",").filter(|s| !s.is_empty())
     .collect();
+  let algstr = alg.as_str();
+
+
+    println!("{:?}", vec);
+match algstr {
+  "rust" => rust_sort(vec),
+  _ => return Json(Ping { 
+    response: format!("Error: couldn't find sorting algorithm {}", algstr),
+    status: "400".to_string()}) 
+}
+}
+fn rust_sort(vec: Vec<&str>) -> Json<Ping>{
     let mut sorted: Vec<i32> = vec![];
     for i in vec.iter() {
     let num = i.parse::<i32>().unwrap(); 
     sorted.push(num);
     }
-    println!("{:?}", vec);
+    sorted.sort();
     let response = Ping {
-    response: format!("{:?}",sorted),
+    response: format!("{:?}", sorted),
     status: "200".to_string(),
-};
+  };
 Json(response)
 }
