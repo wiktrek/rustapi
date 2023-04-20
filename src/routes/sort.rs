7437e921c3cdefdc689d5dd3f31a8ed552145141
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-20 13:26:11 +0200

    simple sort

--- src/routes/sort.rs
+++ src/routes/sort.rs
@@ -9,23 +9,41 @@ pub struct Ping {
 pub fn sort(alg: String, data: String) -> Json<Ping>{
   let vec: Vec<&str> = data.split(",").filter(|s| !s.is_empty())
     .collect();
+  let mut vec_i32: Vec<i32> = vec![];
   let algstr = alg.as_str();
-
-
-    println!("{:?}", vec);
-match algstr {
-  "rust" => rust_sort(vec),
-  _ => return Json(Ping { 
-    response: format!("Error: couldn't find sorting algorithm {}", algstr),
-    status: "400".to_string()}) 
-}
-}
-fn rust_sort(vec: Vec<&str>) -> Json<Ping>{
-    let mut sorted: Vec<i32> = vec![];
     for i in vec.iter() {
     let num = i.parse::<i32>().unwrap(); 
-    sorted.push(num);
+    vec_i32.push(num);
     }
+    println!("{:?}", vec);
+match algstr {
+  "rust" => rust_sort(vec_i32),
+  "simple" => simple_sort(vec_i32),
+  _ => return Json(Ping { 
+    response: format!("Error: couldn't find sorting algorithm {}", algstr),
+    status: "400".to_string()})
+}
+}
+fn simple_sort(vec: Vec<i32>) -> Json<Ping> {
+  let n: i32 = vec.len() as i32;
+let mut i: i32 = 0;
+  while i < n {
+  let mut j = i + 1;
+    while j <n {
+      j+= 1;
+      if vec[j as usize]<vec[i as usize] {
+        
+      }
+    }
+  i += 1;
+  }
+  Json(Ping {
+    response: format!("response"),
+    status: "200".to_string()
+  })
+}
+fn rust_sort(vec: Vec<i32>) -> Json<Ping>{
+  let mut sorted = vec;
     sorted.sort();
     let response = Ping {
     response: format!("{:?}", sorted),
