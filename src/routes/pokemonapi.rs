7c2ab75ff687349b5f277285939f57749fb61a05
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-23 01:53:52 +0200

    pokemon

--- src/routes/pokemonapi.rs
+++ src/routes/pokemonapi.rs
@@ -1,7 +1,10 @@
 use reqwest::*;
-#[get("/pokemon/<pokemon>")]
-pub fn pokemon(pokemon: String) -> String {
+use rocket::serde::json::to_string;
+use std::{io::Error, result::Result};
+#[get("/pokemon/<option>/<pokemon>")]
+pub async fn pokemon(option: String, pokemon: String) -> Result<String, Error> {
     let url = format!("https://pokeapi.co/api/v2/pokemon/{}", pokemon);
-
-    return url.to_string();
+    let response = get(url).await.unwrap().text().await;
+    println!("{:?}", response);
+    Ok("ez".to_string())
 }
