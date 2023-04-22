6e870d994f0db6c682c90059e5b1789cabd26a14
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-23 01:56:15 +0200

    pokemonapi.rs

--- src/routes/pokemonapi.rs
+++ src/routes/pokemonapi.rs
@@ -6,5 +6,10 @@ pub async fn pokemon(option: String, pokemon: String) -> Result<String, Error> {
     let url = format!("https://pokeapi.co/api/v2/pokemon/{}", pokemon);
     let response = get(url).await.unwrap().text().await;
     println!("{:?}", response);
+    match option.as_str() {
+        "height" => height(),
+        _ => return Ok("WRONG OPTION".to_string()),
+    }
     Ok("ez".to_string())
 }
+fn height() {}
