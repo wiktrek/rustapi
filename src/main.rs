c0de24c5b312e2b9a7b76d83c448a9d55ca6eeea
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-23 01:26:50 +0200

    .

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,4 @@ use routes::*;
 #[launch]
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index_rs, ping, test, redirect_rs, wiktrek, json_example, sort_rs, random_rs]).register("/",catchers![internal_error, not_found])
-
 }
--- src/routes/sort_algs/merge.rs
+++ src/routes/sort_algs/merge.rs
@@ -1,6 +1,7 @@
 use rocket::serde::json::Json;
 use super::Ping;
 pub fn merge_sort(mut v: Vec<i32>) -> Json<Ping> {
+  // https://www.youtube.com/watch?v=l7-f9gS8VOs
     Json(Ping {
     response: format!("response"),
     status: "200".to_string()
