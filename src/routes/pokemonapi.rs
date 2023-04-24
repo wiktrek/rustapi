e03bccd62cf764338b3dab3485b9daba557a2718
wiktrek <61013382+wiktrek@users.noreply.github.com>
2023-04-24 21:29:08 +0200

    .

--- src/routes/pokemonapi.rs
+++ src/routes/pokemonapi.rs
@@ -4,16 +4,17 @@ use std::{io::Error, result::Result};
 #[get("/pokemon/<option>/<pokemon>")]
 pub async fn pokemon(option: String, pokemon: String) -> Result<String, std::io::Error> {
     let url = format!("https://pokeapi.co/api/v2/pokemon/{}", pokemon);
-
+    get_response(url, option);
     Ok("ez".to_string())
 }
-fn get_response() {
+async fn get_response(url: String, option: String) -> Result<String, std::io::Error> {
     let response = get(url).await.unwrap().text().await;
     println!("{:?}", response);
     match option.as_str() {
         "height" => height(),
         _ => return Ok("WRONG OPTION".to_string()),
     }
+    Ok("response".to_string())
 }
 fn height() {
     // get height from api
