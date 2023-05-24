5e4c035fe34c27b4e090961d5a10377eb462b243
wiktrek <wiktor.janiszewski10@gmail.com>
2023-05-24 21:01:18 +0200

    .

--- src2/api/mod.rs
+++
@@ -1,6 +0,0 @@
-mod user_api;
-pub use user_api::create_user;
-pub use user_api::delete_user;
-pub use user_api::get_all_users;
-pub use user_api::get_user;
-pub use user_api::update_user;
--- src2/routes/index.rs
+++
@@ -1,6 +0,0 @@
-use rocket::response::content::RawHtml;
-use rocket::*;
-#[get("/")]
-pub fn index() -> RawHtml<&'static str> {
-    RawHtml(include_str!("../data/html/index.html"))
-}
