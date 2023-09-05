0b5295e86caa69f2ab1b100b3dce46aaec413385
wiktrek <wiktor.janiszewski10@gmail.com>
2023-09-05 22:41:58 +0200

    hot take

--- src/api/hottake.rs
+++ src/api/hottake.rs
@@ -19,7 +19,9 @@ pub async fn hot_take() -> Json<Response> {
     let file = fs::read_to_string("./src/data/hottake.json").unwrap();
     let takes: Vec<HotTake> = serde_json::from_str(&file).expect("Error parsing json");
 
-    let take = &takes[0];
+    let num = rand::thread_rng().gen_range(0..takes.len());
+    let take = &takes[num];
+    println!("{}{}", num, takes.len());
     let api_response = Response {
         take: take.take.to_string(),
         author: take.author.to_string(),
